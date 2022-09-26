808c85bc0851276e8faf7b7d9e68727a9d8fa026
Mateusz Szostok <szostok.mateusz@gmail.com>
2022-09-26 14:19:05 +0200

    Simulate change

--- cmd/botkube/main.go
+++ cmd/botkube/main.go
@@ -58,6 +58,7 @@ func main() {
 // run wraps the main logic of the app to be able to properly clean up resources via deferred calls.
 func run() error {
 	// Load configuration
+	fmt.Println("Simulate change for 0.49")
 	config.RegisterFlags(pflag.CommandLine)
 	conf, confDetails, err := config.LoadWithDefaults(config.FromEnvOrFlag)
 	if err != nil {
