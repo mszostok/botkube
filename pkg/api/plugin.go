ece0db601fc8f9cd1c129b2652d029c62b291cd5
Mateusz Szostok <szostok.mateusz@gmail.com>
2022-12-12 19:10:47 +0100

    Add release process for Botkube plugins

--- pkg/api/plugin.go
+++ pkg/api/plugin.go
@@ -10,3 +10,11 @@ var HandshakeConfig = plugin.HandshakeConfig{
 	MagicCookieKey:   "BOTKUBE",
 	MagicCookieValue: "52ca7b74-28eb-4fac-ae79-31a9cbda2454",
 }
+
+// MetadataOutput contains the metadata of a given plugin.
+type MetadataOutput struct {
+	// Version is a version of a given plugin. It should follow the SemVer syntax.
+	Version string
+	// Descriptions is a description of a given plugin.
+	Description string
+}
